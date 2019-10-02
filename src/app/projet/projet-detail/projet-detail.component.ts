8c64a3a3e8d771f90dc8a8014cf2664e165d0379
Anthony Di-persio <dipersio.a@hotmail.com>
2019-10-03 01:11:48 +0200

    ProjetDetailComponent Works

--- src/app/projet/projet-detail/projet-detail.component.ts
+++ src/app/projet/projet-detail/projet-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { DataService } from '../../data.service';
+import { ProjetModel } from '../../models/projet.model';
+import { FormBuilder, FormGroup, Validators, RequiredValidator } from "@angular/forms";
 
 @Component({
   selector: 'app-projet-detail',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjetDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private data: DataService, private fb: FormBuilder) { }
 
   ngOnInit() {
   }
-
+  id;
+  projet:ProjetModel = {
+    Id: 0,
+    Nom: "",
+    DateCreation: "",
+    Duree:"",
+    DateEcheance: "",
+    Contributeurs: "",
+    Description: "",
+    Notes: "",
+    Archive: false
+  };
+  projets: Array<any>;
+  findProjet = () => {
+    this.data.getApi('getProjet/'+this.id).subscribe((res:any)=> {
+      if(res.error){
+        alert('Aucun projet avec cet id');
+      }
+      else {
+        this.projet = res.projet;
+      }
+    })
+  }
 }
